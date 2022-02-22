c522f8bdeed66b91a4ec35ad1f5886d1d7804226
bizzarriA <alice.bizzarri@edu.unife.it>
2022-02-22 11:23:18 +0100

    problema cardinalità

--- main.py
+++ main.py
@@ -15,10 +15,10 @@ if __name__ == '__main__':
     n_train, n_val, n_test = 100, 10, 10
     x_train = [cv2.imread(train_df["filename"][i], cv2.IMREAD_GRAYSCALE) for i in range(n_train)]
     x_train = np.expand_dims(x_train, axis=-1)
-    y_train = tf.one_hot(train_df["label"][:n_train], 3)
-    # print(x_train, y_train)
+    y_train = tf.keras.utils.to_categorical(train_df["label"][:n_train], 3)
+    print(np.shape(x_train), np.shape(y_train))
     x_val = [cv2.imread(val_df["filename"][i], 0) for i in range(n_val)]
-    y_val = tf.one_hot(val_df["label"][:n_val], 3)
+    y_val = tf.keras.utils.to_categorical(val_df["label"][:n_val], 3)
     train_loader = tf.data.Dataset.from_tensor_slices((x_train, y_train))
     validation_loader = tf.data.Dataset.from_tensor_slices((x_val, y_val))
     batch_size = 1
@@ -29,7 +29,7 @@ if __name__ == '__main__':
         validation_loader.shuffle(len(x_val)).batch(batch_size).prefetch(2)
     )
     model = get_model(width=np.shape(x_train[0])[0], height=np.shape(x_train[0])[0])
-    model.summary()
+    # model.summary()
 
     optimizer = tf.keras.optimizers.Adam(0.001)  # * hvd.size())
     print("[INFO] Model compile")
@@ -38,11 +38,12 @@ if __name__ == '__main__':
         optimizer=optimizer,
         metrics=['acc'],
     )
-
+    print("Shape x and y train ",np.shape(x_train), np.shape(y_train))
     model.fit(
         x_train, y_train,
         validation_data=(x_val, y_val),
         epochs=5,
+        # steps_per_epoch=50,
         batch_size=batch_size,
         shuffle=True,
         verbose=1
